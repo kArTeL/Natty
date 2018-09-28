2be507073dde252b642a564db68dad094f2671a8
Neil García <neilgarcia@neils-macbook-pro-2.local>
2018-09-28 14:34:43 -0600

    Initial Commit

---
+++ Natty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Natty
+//
+//  Created by Neil García on 9/28/18.
+//  Copyright © 2018 Natty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
